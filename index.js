12d0c660289bc0a85429c3368aba89918cb170f6
Yesu_Vasupalli <yesu191125@gmail.com>
2025-07-20 08:48:46 +0530

    index.js

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function handleClick(){
     const url = document.getElementById("url").value;
     const img = document.getElementById("image")
     img.src = url;
+    // const text = document.getElementById("textenter")
+    // text.textContent = url;     for the appeariance of entered text or url
+
 }
